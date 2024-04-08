06444a4156f12938bb34c8fdaab66286b62624c9
guolong <565169745@qq.com>
2024-04-08 18:08:36 +0800

    [feat]: 删除无效文件

--- src/org/manci/Main.groovy
+++
@@ -1,9 +0,0 @@
-package org.manci
-
-static void main(String[] args) {
-    println "Hello world!"
-    // 使用示例
-    def gitee = new GiteeApi(null, "a7145574cb3688f44dde322cdb50a68e", "guojongg/manci", "3", "ManCI")
-    def resp = gitee.comment("test-c")
-    println(resp)
-}
